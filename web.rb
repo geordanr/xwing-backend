a5664dd0ac06d72e1f3f0e99bf0306e904862f0d
Geordan Rosario <geordan@doselab.com>
2013-01-13 00:57:59 -0800

    Session auth

--- web.rb
+++ web.rb
@@ -7,22 +7,31 @@ require 'omniauth'
 require 'omniauth-google-oauth2'
 require 'omniauth-facebook'
 
+PROVIDERS = {
+    :google_oauth2 => [ ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''} ],
+    :facebook => [ ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'] ],
+}
+
 class OAuthDemo < Sinatra::Base
     use Rack::Session::Cookie
     use OmniAuth::Builder do
-        provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {access_type: 'online', approval_prompt: ''}
-        provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+        PROVIDERS.each do |provider_name, provider_args|
+            provider provider_name, *provider_args
+        end
     end
+    set :session_secret, ENV['SESSION_SECRET']
 
     get '/' do
         @time = Time.now
+        @providers = PROVIDERS.keys
         haml :index
     end
 
     # Support both GET and POST for callbacks
     %w(get post).each do |method|
         send(method, "/auth/:provider/callback") do
-            env['omniauth.auth'].to_s # => OmniAuth::AuthHash
+            session[:u] = "#{env['omniauth.auth']['provider']}-#{env['omniauth.auth']['uid']}"
+            redirect to('/protected')
         end
     end
 
@@ -31,6 +40,10 @@ class OAuthDemo < Sinatra::Base
     end
 
     get '/protected' do
-        "It's a secret to everyone!"
+        if session.has_key? :u
+            "It's a secret to everyone!"
+        else
+            redirect to('/')
+        end
     end
 end
